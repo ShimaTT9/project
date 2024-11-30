8a15767e323abf42cec59ce57d22fb0c7a8b7127
ShimaTT9 <kornnnrzerza2547@gmail.com>
2024-11-30 23:59:31 +0700

    ⚡️ strongggggg

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>hello world
-      
+    <div>
+      You can do it bro. If it cant destroy you, it'll amke you stronger
     </div>
   );
 }
